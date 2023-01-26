a7f85bb091080fe56779ef092a642fec411089c8
carrichi <roberto@carrichi.com>
2023-01-26 16:17:48 -0600

    Add animation move to top

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const header_offset = $('header').offsetHeight + 10
 
 const for_you = $('to-section-for-you')
 for_you.addEventListener('click',() => { 
+  $('navbar').classList.toggle('open')
   window.scrollTo({
     top: window.scrollY + $('personal-products').getBoundingClientRect().top - header_offset, 
     behavior: 'smooth'
@@ -11,6 +12,7 @@ for_you.addEventListener('click',() => {
 
 const for_professional = $('to-section-for-professional')
 for_professional.addEventListener('click',() => { 
+  $('navbar').classList.toggle('open')
   window.scrollTo({
     top: window.scrollY + $('professional-products').getBoundingClientRect().top - header_offset, 
     behavior: 'smooth'
@@ -18,8 +20,16 @@ for_professional.addEventListener('click',() => {
 
 const for_enterprise = $('to-section-for-enterprise')
 for_enterprise.addEventListener('click',() => { 
+  $('navbar').classList.toggle('open')
   window.scrollTo({
     top: window.scrollY + document.getElementById('enterprise-products').getBoundingClientRect().top - header_offset, 
+    behavior: 'smooth'
+  })})
+
+const move_to_top = $('logo-header')
+move_to_top.addEventListener('click',() => { 
+  window.scrollTo({
+    top: 0,
     behavior: 'smooth'
   })})
 
