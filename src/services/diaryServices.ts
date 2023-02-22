46b506a58639613180a96739f5fad1bf65ba31f7
carrichi <robertocarrichiunam@gmail.com>
2023-02-21 23:06:53 -0600

    integrate dev branch

---
+++ src/services/diaryServices.ts
@@ -0,0 +1,10 @@
+import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../types'
+import diaryData from './diaries.json'
+
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
+
+export const getEntries = (): DiaryEntry[] => diaries
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => diaries
+
+export const addEntry = (): undefined => undefined
