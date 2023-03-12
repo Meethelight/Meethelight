83e8623f02647725ced2c34ddb073002841118f8
carrichi <roberto@carrichi.com>
2023-03-12 03:54:47 -0600

    Correct changes of Next!

---
+++ next/app/[username]/page.js
@@ -0,0 +1,5 @@
+export default function User({params}) {
+  const {username} = params
+
+  return <h1>Username: {username}</h1>
+}
---
+++ next/app/about/page.js
@@ -0,0 +1,22 @@
+const fetchExample = () => {
+  return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+}
+
+export default async function About({ params }) {
+  const posts = await fetchExample()
+
+  return (
+    <>
+      <h1>About - Related Posts</h1>
+      <section>
+        {posts.slice(0,5).map(post => (
+          <article key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </article>
+        ))}
+      </section>
+    </>
+  )
+}
