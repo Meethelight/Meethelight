8de5037840ea804803e47a3fb14a89b13ab93c54
carrichi <roberto@carrichi.com>
2023-03-13 23:44:14 -0600

    Add material Tailwind!

--- next/app/page.js
+++ next/app/page.js
@@ -1,3 +1,4 @@
+import { Button } from '@material-tailwind/react'
 import { Raleway } from 'next/font/google'
 
 const raleway = Raleway({ subsets: ['latin'], weight: ['700'], variable: '--raleway-font' })
@@ -5,9 +6,10 @@ const raleway = Raleway({ subsets: ['latin'], weight: ['700'], variable: '--rale
 export default function Home () {
   return (
     <main>
-      <h1 className='text-5xl font-bold text-cyan-300'>
+      <h1 className={`${raleway.className} text-5xl font-bold text-cyan-300`}>
         Hello world Next.js for Meethelight!
       </h1>
+      <Button>Hello!</Button>
     </main>
   )
 }
--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require('@material-tailwind/react/utils/withMT')
+
+module.exports = withMT({
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
@@ -103,4 +105,4 @@ module.exports = {
     }
   },
   plugins: []
-}
+})
