1c32c4ab4aeac7cd18ef9f638edb0f06c2149efb
carrichi <roberto@carrichi.com>
2023-03-16 23:48:24 -0600

    Modify navbar with custom buttons

--- next/components/layout/Navigation.js
+++ next/components/layout/Navigation.js
@@ -1,51 +1,41 @@
 import Image from 'next/image'
 import styles from './Navigation.module.css'
 import { Pacifico } from 'next/font/google'
+import Logo from '../../public/logo.png'
+import CustomButton from '../misc/CustomButton'
 
 const pacifico = Pacifico({ weight: '400', subsets: ['latin'] })
 
 export default function Navigation () {
   const leftLinks = (
-    <ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
-      <li color='blue-gray' className='p-1 font-normal'>
-        <a id="to-section-for-you" className="button header-link">Para ti</a>
-      </li>
-      <li color='blue-gray' className='p-1 font-normal'>
-        <a id="to-section-for-professional" className="button header-link">Psicólogos</a>
-      </li>
-      <li color='blue-gray' className='p-1 font-normal'>
-        <a id="to-section-for-enterprise" className="button header-link">Empresas</a>
-      </li>
-    </ul>
+    <div className='flex flex-row sm:flex-col lg:items-center gap-2 lg:gap-6 mb-4 mt-2 lg:mb-0 lg:mt-0 '>
+      <CustomButton variant='text' color='indigo' text='Empresas'/>
+      <CustomButton variant='text' color='indigo' text='Empresas'/>
+      <CustomButton variant='text' color='indigo' text='Empresas'/>
+    </div>
   )
 
   const rightLinks = (
-    <ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
-      <li>
-        <a
-          id={`${styles.login_button}`}
-          className={`${styles.button} ${styles.header_link}`}
-          href="#"
-        >Inicia sesión</a>
-      </li>
-      <li>
-        <a
-          id={`${styles.register_button}`}
-          className={`${styles.button} ${styles.header_link}`}
-          href="#"
-        >Regístrate</a>
-      </li>
-    </ul>
+    <div className='flex flex-row lg:flex-row gap-2 items-end lg:gap-6 mb-4 mt-2 lg:mb-0 lg:mt-0'>
+      <CustomButton variant='outlined' color='indigo' text='Inicia sesión'/>
+      <CustomButton color='indigo' text='Regístrate'/>
+    </div>
   )
 
   return (
-    <nav className='flex w-full'>
-      <div className='flex gap-3'>
-        <Image width={25} height={25} src={''} alt="logo"/>
-        <p className={`${pacifico.className} text-5xl`}>Meethelight</p>
+    <nav className='w-full flex'>
+      <div>
+        <Image
+          src={Logo}
+          alt='Meethelight Logo'
+          width='40'
+          height='40'
+          className='inline-block'
+        />
+        <p className={`${pacifico.className} inline-block text-3xl`}>Meethelight</p>
       </div>
       <div className='flex'>
-        <div className={`${styles.menu} ${styles.menu_left} bg-indigo-500`}>{leftLinks}</div>
+        <div className={`${styles.menu} ${styles.menu_left}`}>{leftLinks}</div>
         <div className={`${styles.menu} ${styles.menu_right}`}>{rightLinks}</div>
       </div>
     </nav>
