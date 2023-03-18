3ea5d8d7c106534944c57c054193cf840caf9523
carrichi <roberto@carrichi.com>
2023-03-17 23:06:50 -0600

    Add some styles in navbar

--- next/app/page.js
+++ next/app/page.js
@@ -8,7 +8,7 @@ export default function Home () {
   return (
     <main>
       <Navigation />
-      <h1 className={`${raleway.className} text-5xl font-bold text-cyan-300 bg-gray-100`}>
+      <h1 className={`${raleway.className} h-full text-5xl font-bold text-cyan-300 bg-gray-100`}>
         Hello world Next.js for Meethelight!
       </h1>
       <CustomButton text='Hola!' color='indigo' />
--- next/components/layout/Navigation.js
+++ next/components/layout/Navigation.js
@@ -4,13 +4,13 @@ import { Pacifico } from 'next/font/google'
 import Logo from '../../public/logo.png'
 import CustomButton from '../misc/CustomButton'
 
-const pacifico = Pacifico({ weight: '400', subsets: ['latin'] })
+const pacifico = Pacifico({ weight: '400', subsets: ['latin'], style: 'normal' })
 
 export default function Navigation () {
   const leftLinks = (
     <div className='flex flex-row sm:flex-col lg:items-center gap-2 lg:gap-6 mb-4 mt-2 lg:mb-0 lg:mt-0 '>
-      <CustomButton variant='text' color='indigo' text='Empresas'/>
-      <CustomButton variant='text' color='indigo' text='Empresas'/>
+      <CustomButton variant='text' color='indigo' text='Para ti'/>
+      <CustomButton variant='text' color='indigo' text='Psicólogos'/>
       <CustomButton variant='text' color='indigo' text='Empresas'/>
     </div>
   )
@@ -23,7 +23,7 @@ export default function Navigation () {
   )
 
   return (
-    <nav className='w-full flex'>
+    <nav className='w-full flex absolute space-x-4 bg-white'>
       <div>
         <Image
           src={Logo}
@@ -34,9 +34,9 @@ export default function Navigation () {
         />
         <p className={`${pacifico.className} inline-block text-3xl`}>Meethelight</p>
       </div>
-      <div className='flex'>
-        <div className={`${styles.menu} ${styles.menu_left}`}>{leftLinks}</div>
-        <div className={`${styles.menu} ${styles.menu_right}`}>{rightLinks}</div>
+      <div className='flex items-center'>
+        <div className={`${styles.menu} ${styles.menu_left} inline-block`}>{leftLinks}</div>
+        <div className={`${styles.menu} ${styles.menu_right} inline-block`}>{rightLinks}</div>
       </div>
     </nav>
   )
